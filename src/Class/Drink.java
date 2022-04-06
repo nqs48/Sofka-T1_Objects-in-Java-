1c01d9a55f4215d8535a779e29568d49b359cb1e
Nestor Quiroga Suárez <nqs48@icloud.com>
2022-04-06 17:34:40 -0500

    Add Documentation class Drink

--- src/Class/Drink.java
+++ src/Class/Drink.java
@@ -7,27 +7,27 @@ package Class;
 public class Drink {
 
     /**
-     *
+     * Drink's name
      */
     private String name;
 
     /**
-     *
+     * Drink's weight
      */
     private float averageWeight;
 
     /**
-     *
+     * Drink's price
      */
     private float price;
 
     /**
-     *
+     * Drink's validation type, can be true or false
      */
     private boolean alcoholic;
 
     /**
-     *
+     * Drink's category type Section, is a object type section
      */
     public Section category;
 
@@ -35,48 +35,48 @@ public class Drink {
     //Methods Get and Set
 
     /**
-     *
-     * @return
+     * Get the current drink's name
+     * @return returns the drink's name type string
      */
     public String getName() {
         return name;
     }
 
     /**
-     *
-     * @param name
+     * Set the new drink's name
+     * @param name Contains the new drink's name, type string
      */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
-     *
-     * @return
+     * Get the current drink's type
+     * @return returns the drink's type, return a boolean value
      */
     public boolean isAlcoholic() {
         return alcoholic;
     }
 
     /**
-     *
-     * @param alcoholic
+     * Set the new drink's type, it's boolean
+     * @param alcoholic Contains the new drink's type, receive a boolean value
      */
     public void setAlcoholic(boolean alcoholic) {
         this.alcoholic = alcoholic;
     }
 
     /**
-     *
-     * @return
+     * Get the current drink's price
+     * @return returns the drink's price type float
      */
     private float getPrice() {
         return price;
     }
 
     /**
-     *
-     * @param price
+     * Set the new drink's price
+     * @param price Contains the new price type float
      */
     private void setPrice(float price) {
         this.price = price;
